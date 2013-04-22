b901d36309345b18a798e2f0b5eac7f255165eb6
dtan4 <dtan4@arch>
2013-04-22 11:45:22 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snipstock::Application.config.secret_token = 'c1904e75b6d001f50620aed16bbbb17c0ffabc3847baf9665e806ade0e3e377637e759d38cfaf52b4d5708f46a26b7ee550b323df34c5d645be83cec84cdf8c1'
