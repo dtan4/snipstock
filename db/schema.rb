a271fd722c538b3705137744a6a0d237c062970d
dtan4 <dtan4@arch>
2013-04-24 16:44:46 +0900

    Add unique constraint to name of user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130422032640) do
+ActiveRecord::Schema.define(:version => 20130424071128) do
 
   create_table "snippets", :force => true do |t|
     t.string   "lang"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(:version => 20130422032640) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
 
 end
