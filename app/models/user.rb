5917e20b621e7ba4ee3a458b454e522e20b28daa
dtan4 <dtan4@arch>
2013-04-24 15:43:18 +0900

    Implement new user addition

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
-  attr_accessible :name, :password_digest
+  attr_accessible :name, :password_digest, :password, :password_confirmation
+
+  validates :name, presence: true, uniqueness:true, format: {
+    with: /^[0-9a-zA-Z]+$/,
+    message: "は半角英数字で入力してください"
+  }
+  validates :password, length: { minimum: 8 }
+  has_secure_password
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user name must be composed of numerics and alphabets" do
+    user01 = User.new(name: "*?", password: "hogehoge",
+                    password_confirmation: "hogehoge")
+    assert user01.invalid?
+    assert user01.errors[:name].any?
+
+    user02 = User.new(name: "aa*/34", password: "hogehoge",
+                      password_confirmation: "hogehoge")
+    assert user02.invalid?
+    assert user02.errors[:name].any?
+
+    user03 = User.new(name: "123abAB", password: "hogehoge",
+                    password_confirmation: "hogehoge")
+    assert user03.valid?
+  end
+
+  test "password must be at least 8 characters" do
+    user01 = User.new(name: "hoge", password: "hoge",
+                    password_confirmation: "hoge")
+    assert user01.invalid?
+    assert user01.errors[:password].any?
+
+    user02 = User.new(name: "hogehoge", password: "hogehoge",
+                    password_confirmation: "hogehoge")
+    assert user02.valid?
+  end
 end
