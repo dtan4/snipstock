71a10428b6e9e5e80e0ec29f15cc8c1113832417
dtan4 <dtan4@mac>
2013-05-02 01:39:21 +0900

    Create user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @snippets = Snippet.where(user_id: @user.id).reverse
 
     respond_to do |format|
       format.html # show.html.erb
