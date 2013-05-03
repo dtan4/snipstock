53143b48e42ed53848e86ac06c76e0bd351364b6
dtan4 <dtan4@arch>
2013-05-03 19:02:59 +0900

    Add private column to snippets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428085800) do
+ActiveRecord::Schema.define(:version => 20130503100142) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20130428085800) do
     t.string   "title",       :default => "no title", :null => false
     t.text     "description"
     t.integer  "user_id"
+    t.boolean  "private"
   end
 
   create_table "taggings", :force => true do |t|
